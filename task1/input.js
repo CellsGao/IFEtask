7840c8caf3e4f4f53b5f8911d35eab0629d6c8a5
Cellsgao <326422808@qq.com>
2017-02-26 11:04:12 +0800

    添加注释

--- task1/input.js
+++ task1/input.js
@@ -2,7 +2,7 @@
  * Created by gaoshang on 2017/2/26.
  */
 function input(){
-    var text=document.getElementById("aqi-input");
-    var replaceWords=document.getElementById("aqi-display").firstChild;
-    replaceWords.nodeValue=text.value;
+    var text=document.getElementById("aqi-input"); //获取输入框内的文本
+    var replaceWords=document.getElementById("aqi-display").firstChild; //获取需要被替换的文本
+    replaceWords.nodeValue=text.value;  //将文本替换
 }
